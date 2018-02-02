6ea6895ec0d8b0e04b09d70d4867b9d685e4e06b
Poornendulal Venugopalan <kevinibmer@gmail.com>
2018-02-02 12:32:58 +0530

    test

--- src/demo/TC01.java
+++ src/demo/TC01.java
@@ -1,14 +1,20 @@
 package demo;
 
+import org.openqa.selenium.By;
+
 public class TC01 {
-	public static void main(String[] args) {
-		System.setProperty("webdriver.firefox.marionette","");
-		char operation[]= {'i','i','b','d','v','a','r','v'};
-        String control[]= {"userName", "password","submit","dropdownid","divverify","Flights","tripType","verify"};
-      	String testdata[] = {"username","password","Option1","div","Verification"};
-      	Initialize init = new Initialize("c:\\index.html");
-      	Automation automate = new Automation();
-      	automate.run(init.driver, operation, control, testdata);
-      	init.driver.quit();
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.ie.driver", "C:\\driver\\IEDriverServer.exe");
+		char operation[] = { 'i', 'i', 'b', 'd', 'v', 'a', 'r', 'v' };
+		String control[] = { "userName", "password", "submit", "dropdownid", "divverify", "Flights", "tripType",
+				"verify" };
+		String testdata[] = { "username", "password", "Option1", "div", "Verification" };
+		Initialize init = new Initialize("http://www.yahoo.com");
+		
+		System.out.println(init.driver);
+		System.out.println("did i wait ?");
+		/*Automation automate = new Automation();
+		automate.run(init.driver, operation, control, testdata);
+		init.driver.quit();*/
 	}
 }
