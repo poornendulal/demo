37370879301b603891985db3fbb33d87a8e2c40b
Poornendulal Venugopalan <kevinibmer@gmail.com>
2018-02-02 15:31:35 +0530

    sdf

--- src/demo/Initialize.java
+++ src/demo/Initialize.java
@@ -4,10 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Initialize {
-	protected WebDriver driver = new InternetExplorerDriver();
+	protected WebDriver driver = new HtmlUnitDriver();
 	public Initialize(String URL) throws InterruptedException {
 		System.out.println("test");
 		//driver.manage().window().setSize(new Dimension(500,500));
