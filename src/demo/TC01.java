7f678e788902cc9424e488720f1d1442678a34b0
PoornendulalVenugopa <PoornendulalVenugopa@9.145.58.148>
2018-03-14 16:23:13 +0000

    Simple hellow world to see if this one is working

--- src/demo/TC01.java
+++ src/demo/TC01.java
@@ -2,15 +2,6 @@ package demo;
 
 public class TC01 {
 	public static void main(String[] args) {
-		System.setProperty("webdriver.ie.driver", "C:\\driver\\IEDriverServer.exe");
-		char[] operation = { 'i', 'i', 'b', 'd', 'v', 'a', 'r', 'v' };
-		String[] control = { "userName", "password", "submit", "dropdownid", "divverify", "Flights", "tripType",
-				"verify" };
-		String[] testdata = { "username", "password", "Option1", "div", "Verification" };
-		Initialize init = new Initialize("http://www.yahoo.com");
-		
-		Automation automate = new Automation();
-		automate.run(init.driver, operation, control, testdata);
-		init.driver.quit();
+		System.out.println("hellow ");
 	}
 }
