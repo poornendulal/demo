632ad5250a89587f5ad62f21f30dd3c87c10a8c9
PoornendulalVenugopa <PoornendulalVenugopa@9.145.5.229>
2018-03-15 10:50:30 +0000

    replaced stringbuilder into sysout for generating sonarqube error

--- src/demo/Automation.java
+++ src/demo/Automation.java
@@ -8,46 +8,46 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Automation {
 	public boolean run(WebDriver driver, char[] operation, String[] control, String[] testdata) {
-		StringBuilder log = new StringBuilder();
+		//StringBuilder log = new StringBuilder();
 		try {
 
 			int j = 0;
 			int i = 0;
 			int k = 0;
 			for (i = 0; i < operation.length; i++) {
-				log.append("Loop counters value i=" + i + " and j=" + j + " and k=" + k);
+				System.out.println("Loop counters value i=" + i + " and j=" + j + " and k=" + k);
 				if (operation[i] == 'i') {
 					WebDriverWait waiter = new WebDriverWait(driver, 30);
 					WebElement webElement1a = waiter
 							.until(ExpectedConditions.visibilityOfElementLocated(By.id(control[j++])));
-					log.append(webElement1a.getAttribute("Type")
+					System.out.println(webElement1a.getAttribute("Type")
 							+ " Type identified. SendKeys command will be performed with '" + testdata[k]);
 					webElement1a.sendKeys(testdata[k++]);
 				} else if (operation[i] == 'b') {
 					WebDriverWait waiter = new WebDriverWait(driver, 30);
 					WebElement webElement1a = waiter
 							.until(ExpectedConditions.visibilityOfElementLocated(By.name(control[j++])));
-					log.append(
+					System.out.println(
 							webElement1a.getAttribute("Type") + " Type identified. Click command will be performed.");
 					webElement1a.click();
 				} else if (operation[i] == 'a') {
 					WebDriverWait waiter = new WebDriverWait(driver, 30);
 					WebElement webElement1a = waiter
 							.until(ExpectedConditions.visibilityOfElementLocated(By.linkText(control[j++])));
-					log.append(webElement1a.getTagName() + " control identified. Click command will be processed.");
+					System.out.println(webElement1a.getTagName() + " control identified. Click command will be processed.");
 					webElement1a.click();
 				} else if (operation[i] == 'r') {
 					WebDriverWait waiter = new WebDriverWait(driver, 30);
 					WebElement webElement1a = waiter
 							.until(ExpectedConditions.visibilityOfElementLocated(By.name(control[j++])));
-					log.append(
+					System.out.println(
 							webElement1a.getAttribute("Type") + " Type identified. Click command will be processed.");
 					webElement1a.click();
 				} else if (operation[i] == 'd') {
 					WebDriverWait waiter = new WebDriverWait(driver, 30);
 					WebElement webElement1a = waiter
 							.until(ExpectedConditions.visibilityOfElementLocated(By.id(control[j++])));
-					log.append(
+					System.out.println(
 							webElement1a.getTagName() + " control identified. Select command will be performed with '"
 									+ testdata[k] + "' test data.");
 				} else if (operation[i] == 'v') {
@@ -55,18 +55,18 @@ public class Automation {
 					WebElement webElement1a = waiter
 							.until(ExpectedConditions.visibilityOfElementLocated(By.id(control[j++])));
 
-					log.append(webElement1a.getTagName() + " control identified. Verification will be performed with '"
+					System.out.println(webElement1a.getTagName() + " control identified. Verification will be performed with '"
 							+ testdata[k] + "' test data.");
 					if (webElement1a.getText().contains(testdata[k])) {
-						log.append(webElement1a.getText() + " contains the test data " + testdata[k]);
+						System.out.println(webElement1a.getText() + " contains the test data " + testdata[k]);
 					} else {
-						log.append(webElement1a.getText() + " does not contain the test data " + testdata[k]);
+						System.out.println(webElement1a.getText() + " does not contain the test data " + testdata[k]);
 					}
 				}
 			}
 			return true;
 		} catch (Exception e) {
-			log.append("Unhandled Exception occured ! " + e.toString());
+			System.out.println("Unhandled Exception occured ! " + e.toString());
 			return false;
 		}
 	}
